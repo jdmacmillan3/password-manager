2d3eba5a52a553576fb6422abf74f03d61f5d56d
John Davis MacMillan <jdmacmil@ncsu.edu>
2023-10-11 10:03:05 -0400

    Updated to connect front and back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import PasswordDatabase from './apis/PasswordDatabase';
+import { useState } from 'react';
 
 function App() {
+
+  const [password, setPassWord] = useState('')
+  const [account, setAccount] = useState('')
+
+  const addPassword = async (e) => {
+    e.preventDefault();
+    try{
+      await PasswordDatabase.post(`/addPassword`, {
+        password: password, 
+        account: account,
+      });
+    } catch (error){
+      console.log(error.response.data);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className = "AddPassword"> 
+        <input 
+          type = "text" 
+          placeholder = "Ex. password123" 
+          onChange={(event) => {
+            setPassWord(event.target.value);
+          }}
+        />
+        <input 
+          type = "text" 
+          placeholder = "Ex. Facebook"
+          onChange={(event) => {
+            setAccount(event.target.value);
+          }}
+        />
+        <button onClick={addPassword}> Add Password</button>
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,10 +2,74 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3005;
+const cors = require('cors');
 const db = require ('./db');
 
-app.get(`/`, (req, res) => {
-    res.send("Hello World");
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+// app.post(`/addPassword`, (req, res, next) => {
+//     try{
+//     	const {password, account} = req.body
+
+//         db.query(
+//             "INSERT INTO passwords (password, account) VALUES (?, ?)", 
+//             [password, account], 
+//             (err, result) => {
+//                 if (err){
+//                     console.log(err);
+//                     next(err);
+//                 } else{
+//                     res.send("Success");
+//                 }
+//             }
+//         );
+//     } catch(err){
+//         console.log(err.response.data);
+//         next(err)
+//     }
+// });
+
+// app.post(`/addPassword`, async (req, res) => {
+//     console.log('hi');
+//     try{
+//         const {password, account} = req.body
+//         await db.query(
+//             "INSERT INTO passwords (password, account) VALUES (?, ?)", 
+//             [password, account,], 
+//             (err, result) => {
+//                 if (err){
+//                     console.log(err);
+//                 } else{
+//                     res.send("Success");
+//                 }
+//             }
+//         );
+//     } catch(err){
+//         console.log(err);
+//     }
+// });
+
+app.post(`/addPassword`, async (req, res) => {
+    console.log(req.body);
+    try{
+        const results = await db.query(
+            "INSERT INTO passwords (password, account) VALUES ($1, $2) returning *",
+            [req.body.password, req.body.account]);
+        res.status(201).json({
+            status: "Success",
+            data:{
+                password: results.rows[0],
+            }
+        });
+    } catch(err){
+        console.log(err);
+    }
 });
 
 app.listen(PORT, () => {
